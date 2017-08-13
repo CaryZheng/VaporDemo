c14f638d8408f05b9a93102c020f7c96ffb6b2bc
CaryZheng <CaryZhengMy@gmail.com>
2017-08-13 20:25:55 +0800

    Update Routes

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -10,33 +10,27 @@ final class Routes: RouteCollection {
     }
     
     func build(_ builder: RouteBuilder) throws {
-        builder.get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
         
-         builder.get("leafimage") { req in
-             return try self.view.make("welcome")
-         }
-
-        builder.get("leaftext") { req in
+        let leafGroup = builder.grouped("leaf")
+        leafGroup.get("text") { req in
             return try self.view.make("hello", [
                     "name": "Cary"
                 ])
         }
-
-        builder.get("plaintext") { req in
-            return "Hello, world!"
+        
+        leafGroup.get("image") { req in
+            return try self.view.make("welcome")
         }
         
-        // response to requests to /info domain
-        // with a description of the request
-        builder.get("info") { req in
-            return req.description
-        }
-        
-//        try builder.resource("posts", PostController.self)
+//        builder.get("leafimage") { req in
+//            return try self.view.make("welcome")
+//        }
+//        
+//        builder.get("leaftext") { req in
+//            return try self.view.make("hello", [
+//                "name": "Cary"
+//                ])
+//        }
         try builder.resource("user", UserController.self)
         
         let signinVC = SigninController()
@@ -44,6 +38,24 @@ final class Routes: RouteCollection {
         
         let signupVC = SignupController()
         builder.post("signup", handler: signupVC.create)
+        
+//        builder.get("hello") { req in
+//            var json = JSON()
+//            try json.set("hello", "world")
+//            return json
+//        }
+
+//        builder.get("plaintext") { req in
+//            return "Hello, world!"
+//        }
+        
+        // response to requests to /info domain
+        // with a description of the request
+//        builder.get("info") { req in
+//            return req.description
+//        }
+        
+//        try builder.resource("posts", PostController.self)
     }
 }
 
