108fbaa836c0594bc72e7d268713b91d719964bd
CaryZheng <CaryZhengMy@gmail.com>
2017-08-14 11:26:29 +0800

    Update unit test case

--- Tests/AppTests/RouteTests.swift
+++ Tests/AppTests/RouteTests.swift
@@ -20,13 +20,10 @@ class RouteTests: TestCase {
     func startTestTask() throws {
         try testSignin()
         
-        try testHello()
-        try testInfo()
-        try testPlaintext()
-        
         // leaf
         try testLeaftext()
         try testLeafimage()
+        try testLeafmarkdown()
     }
     
     func testSignin() throws {
@@ -35,7 +32,7 @@ class RouteTests: TestCase {
         try requestData.set("name", "cary")
         try requestData.set("password", "123456")
         
-        let res = try drop.testResponse(to: .post, at: "signin", hostname: hostname, headers: headers, body: requestData.makeBody())
+        let res = try drop.testResponse(to: .post, at: "v1/user/signin", hostname: hostname, headers: headers, body: requestData.makeBody())
         
         // check
         let resData = try JSON(bytes: res.body.bytes!)
@@ -45,36 +42,28 @@ class RouteTests: TestCase {
         XCTAssertEqual(resData["msg"]?.string, "success")
     }
     
-    func testHello() throws {
-        try drop
-            .testResponse(to: .get, at: "hello")
-            .assertStatus(is: .ok)
-            .assertJSON("hello", equals: "world")
-    }
-
-    func testInfo() throws {
-        try drop
-            .testResponse(to: .get, at: "info")
-            .assertStatus(is: .ok)
-            .assertBody(contains: "0.0.0.0")
-    }
-    
-    func testPlaintext() throws {
-        try drop
-            .testResponse(to: .get, at: "plaintext")
-            .assertStatus(is: .ok)
-            .assertBody(equals: "Hello, world!")
-    }
+//    func testInfo() throws {
+//        try drop
+//            .testResponse(to: .get, at: "info")
+//            .assertStatus(is: .ok)
+//            .assertBody(contains: "0.0.0.0")
+//    }
     
     func testLeaftext() throws {
         try drop
-            .testResponse(to: .get, at: "leaftext")
+            .testResponse(to: .get, at: "v1/leaf/text")
             .assertStatus(is: .ok)
     }
     
     func testLeafimage() throws {
         try drop
-            .testResponse(to: .get, at: "leafimage")
+            .testResponse(to: .get, at: "v1/leaf/image")
+            .assertStatus(is: .ok)
+    }
+    
+    func testLeafmarkdown() throws {
+        try drop
+            .testResponse(to: .get, at: "v1/leaf/markdown")
             .assertStatus(is: .ok)
     }
     
