338e80973135524dc64f575061be537d5b5f89bd
CaryZheng <CaryZhengMy@gmail.com>
2018-04-03 13:22:51 +0800

    Add crypto hash

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,7 @@
 import Routing
 import Vapor
 import FluentMySQL
+import Crypto
 
 /// Register your application's routes here.
 ///
@@ -52,16 +53,14 @@ public func routes(_ router: Router) throws {
         return result
     }
     
-    // Example of creating a Service and using it.
-    router.get("hash", String.parameter) { req -> String in
-        // Create a BCryptHasher using the Request's Container
-        let hasher = try req.make(BCryptHasher.self)
-
-        // Fetch the String parameter (as described in the route)
-        let string = try req.parameter(String.self)
-
-        // Return the hashed string!
-        return try hasher.make(string)
+    // Hash
+    router.get("crypto", "hash", String.parameter) { req -> String in
+        let value = try req.parameter(String.self)
+        let hashData = try SHA1.hash(value)
+        
+        let result = hashData.hexEncodedString()
+        
+        return ResponseWrapper(protocolCode: .success, obj: result).makeResponse()
     }
 
     // Example of configuring a controller
