2e96254239ff174c7166066dfec9e1c09adf414f
CaryZheng <CaryZhengMy@gmail.com>
2018-05-02 18:57:39 +0800

    Add protobuf code

--- Package.swift
+++ Package.swift
@@ -7,10 +7,11 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
         // mysql
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.3")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "SwiftProtobuf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -36,6 +36,25 @@ public func routes(_ router: Router) throws {
         return response
     }
     
+    router.get("protobuf") { req -> String in
+        var info = BookInfo()
+        info.id = 1098
+        info.title = "Zzb Test"
+        info.author = "CaryZheng"
+        
+        let binaryData: Data = try info.serializedData()
+        let decodedInfo = try BookInfo(serializedData: binaryData)
+        
+        print("protobuf decodedInfo = \(decodedInfo)")
+        
+        let jsonData: Data = try info.jsonUTF8Data()
+        let receiveFromJSON = try BookInfo(jsonUTF8Data: jsonData)
+        
+        print("protobuf receiveFromJSON = \(receiveFromJSON)")
+        
+        return ResponseWrapper<DefaultResponseObj>(protocolCode: .success).makeResponse()
+    }
+    
     try router.register(collection: UserController())
     try router.register(collection: CryptoController())
     try router.register(collection: HttpController())
