bfee5ecfc4a332e0318761548a08fd9146acd2bc
CaryZheng <CaryZhengMy@gmail.com>
2018-05-05 13:29:30 +0800

    Update Vapor 3 release version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "VaporDemo",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         // mysql
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.3")
